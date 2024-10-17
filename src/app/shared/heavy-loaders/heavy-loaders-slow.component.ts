0488ab4b0e5571f215b3a73bc772df13140d16d7
pablo <pnunfe@gmail.com>
2024-10-17 13:14:56 +0200

    initial project and first commit

---
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  standalone: true,
+  imports: [],
+  template: ` heavy loaders slow works! `,
+})
+export class HeavyLoadersSlowComponent {}
---
+++ src/app/shared/heavy-loaders/user-loader.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: ` user loader works! `,
+})
+export class UserLoaderComponent {}
