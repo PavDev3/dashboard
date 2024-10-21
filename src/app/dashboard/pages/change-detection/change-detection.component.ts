5e3321b35e81ec0dabd0948f7b85f2d6336c169f
pablo <pnunfe@gmail.com>
2024-10-21 12:28:41 +0200

    change detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   standalone: true,
   selector: 'app-change-detection',
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="currentFramework()" />
+
+    <pre>
+    {{ frameworkAsSignal() | json }}
+  </pre
+    >
+
+    <pre>
+      {{ frameworkAsProperty | json }}
+    </pre
+    >
+  `,
   styles: ``,
 })
-export class ChangeDetectionComponent {}
+export class ChangeDetectionComponent {
+  public currentFramework = computed(() => {
+    return `Current Framework: ${this.frameworkAsSignal().name}`;
+  });
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsSignal.update((framework) => {
+        return {
+          ...framework,
+          name: 'React',
+        };
+      });
+      console.log('frameworkAsSignal updated');
+    }, 3000);
+  }
+}
